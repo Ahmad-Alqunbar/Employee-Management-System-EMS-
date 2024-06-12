db7510c6728bf74b21e9c90da13f3eecb71969e0
Ahmad Alqunbar <88638082+Ahmad-Alqunbar@users.noreply.github.com>
2024-06-12 12:04:12 +0300

    Update footer.php

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,12 +1,18 @@
 <?php ?>
 </div>
+
 </div>
+
 </div>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
 
+
 <script>
+
+  
 
 $(document).ready(()=>{
   $('#open-sidebar').click(()=>{
@@ -23,6 +29,7 @@ $(document).ready(()=>{
    });
 });
 
+  
 
 // Reload the page every 2 minutes 
 setInterval(function() {
@@ -56,5 +63,9 @@ setInterval(function() {
   resetInactivityTimeout();
 </script>
 </body>
+
+
+
+
 
 </html>
